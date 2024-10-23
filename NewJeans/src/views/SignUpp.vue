9efacc6ccb294d9ee214664aec607ad2fdcddbc8
DESKTOP-SCJ83VA\user <xx021521@gmail.com>
2024-10-23 17:58:46 +0900

    1023/로그인및회원가입_로직추가

--- NewJeans/src/views/SignUpp.vue
+++ NewJeans/src/views/SignUpp.vue
@@ -73,7 +73,7 @@ const handleSignUp = async () => {
 
     //백엔드로 회원가입 요청 보내기(백엔드에서 url 받아서 집어넣으면 됨)
     const response = await axios.post(
-      'https://your-backend-url.com/api/signup',
+      'http://192.168.0.5:8080/join',
       signUpDate,
     )
 
