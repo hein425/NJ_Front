f095532dc5e7deda24da9cc4f56c137ca2f86863
DESKTOP-SCJ83VA\user <xx021521@gmail.com>
2024-11-21 16:06:14 +0900

    1121

--- NewJeans/src/views/KakaoLoginView.vue
+++ NewJeans/src/views/KakaoLoginView.vue
@@ -1,13 +1,7 @@
 <template>
-    <div>
-        로그인중
-    </div>
+  <div></div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
