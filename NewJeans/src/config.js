d9941c26eec2bbf76d102cb6fa38bbebcbe326b7
DESKTOP-SCJ83VA\user <tackteho@gmail.com>
2024-12-27 12:24:21 +0900

    다이어리 탭에서 다이어리 수정 및 삭제 기능 추가

--- NewJeans/src/config.js
+++ NewJeans/src/config.js
@@ -3,9 +3,9 @@
 // export const BASE_URL = 'http://localhost:50001';
 // export const SCHE_URL = 'http://localhost:50002';
 // export const BASE_URL = 'http://112.222.157.156:10004';
-// export const BASE_URL = 'http://localhost:20010';
+export const BASE_URL = 'http://localhost:20010';
 // export const BASE_URL = 'http://localhost:50003';
-export const BASE_URL = 'http://mafront.ildal.store';
+// export const BASE_URL = 'http://mafront.ildal.store';
 
 
-export const NOTI_BASE_URL = 'http://192.168.0.17:50005';
+// export const NOTI_BASE_URL = 'http://192.168.0.17:50005';
