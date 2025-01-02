01d8149f152bf1f35e879bb2bee111602137ed7d
DESKTOP-SCJ83VA\user <tackteho@gmail.com>
2025-01-02 17:50:56 +0900

    r게임 페이지 추가

--- NewJeans/src/router/index.js
+++ NewJeans/src/router/index.js
@@ -16,6 +16,7 @@ import ThreadView from '@/views/ThreadView.vue';
 import TeamsViewCopy from '@/views/TeamsView copy.vue';
 import TeamsView from '../views/TeamsView.vue';
 import ThreadUserProfile from '@/components/ThreadUserProfile.vue';
+import Games from '@/views/games/games.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -40,6 +41,11 @@ const router = createRouter({
       path: '/teams',
       name: 'teams',
       component: TeamsViewCopy,
+    },
+    {
+      path: '/games',
+      name: 'games',
+      component: Games,
     },
     {
       path: '/thread',
---
+++ NewJeans/src/views/games/StickMan.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="center">
+    <div id="game">
+      <div id="maze">
+        <div id="end"></div>
+      </div>
+      <div id="joystick">
+        <div class="joystick-arrow"></div>
+        <div class="joystick-arrow"></div>
+        <div class="joystick-arrow"></div>
+        <div class="joystick-arrow"></div>
+        <div id="joystick-head"></div>
+      </div>
+      <div id="note">
+        Click the joystick to start!
+        <p>Move every ball to the center. Ready for hard mode? Press H</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import '/src/views/games/1/StickMan.js'; // JS 파일을 import
+
+export default {
+  mounted() {
+    console.log('Game initialized');
+  },
+};
+</script>
+
+<style scoped>
+@import '/src/views/games/1/StickMan.css'; /* CSS 경로 확인 */
+</style>
+
