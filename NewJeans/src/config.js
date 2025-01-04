fa288b387599921a758d8d4c9330faeefe3a851e
DESKTOP-SCJ83VA\user <tackteho@gmail.com>
2025-01-04 12:35:21 +0900

    알림 오게 만듬

--- NewJeans/src/config.js
+++ NewJeans/src/config.js
@@ -9,4 +9,4 @@ export const BASE_URL = 'http://msafront.ildal.store';
 
 // export const USER_BASE_URL = 'http://192.168.0.17:50001';
 // export const CALENDAR_BASE_URL = 'http://192.168.0.17:50002';
-// export const NOTI_BASE_URL = 'http://192.168.0.17:50005';
+// export const BASE_URL = 'http://192.168.0.17:50005';
